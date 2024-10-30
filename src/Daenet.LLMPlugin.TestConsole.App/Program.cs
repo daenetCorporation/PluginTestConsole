4ec467bef60418c9d7657e890163fb5309ac50bb
Damir Dobric <ddobric@daenet.com>
2024-10-30 15:50:25 +0100

    excluded non existing plugin

--- src/Daenet.LLMPlugin.TestConsole.App/Program.cs
+++ src/Daenet.LLMPlugin.TestConsole.App/Program.cs
@@ -101,7 +101,7 @@ namespace Daenet.LLMPlugin.TestConsole.App
         private static void UseSemanticSearchApi(IConfiguration configuration, ServiceCollection serviceCollection)
         {
             //
-            SearchApi.UseSemantSearchApi(configuration, serviceCollection);
+           // SearchApi.UseSemantSearchApi(configuration, serviceCollection);
         }
 
     }
